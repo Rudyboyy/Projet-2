a7aca14951f96353dbf6b99cb3b317e91c3cd056
Rudyboy <rudylepretre@gmail.com>
2022-04-15 17:25:07 +0200

    Modification UserRepository + FakeApiService

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -1,8 +1,13 @@
 package com.openclassrooms.magicgithub.api;
 
-import com.openclassrooms.magicgithub.model.User;
-import java.util.List;
+
 import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.generateUsers;
+import static com.openclassrooms.magicgithub.model.User.random;
+
+import com.openclassrooms.magicgithub.model.User;
+
+import java.util.List;
+
 
 public class FakeApiService implements ApiService {
 
@@ -15,7 +20,7 @@ public class FakeApiService implements ApiService {
     @Override
     public List<User> getUsers() {
         // TODO A modifier
-        return null;
+        return users;
     }
 
     /**
@@ -25,6 +30,7 @@ public class FakeApiService implements ApiService {
     @Override
     public void generateRandomUser() {
         // TODO A modifier
+        users.add(random());
     }
 
     /**
@@ -33,5 +39,9 @@ public class FakeApiService implements ApiService {
     @Override
     public void deleteUser(User user) {
         // TODO A modifier
+        users.remove(user);
     }
+
+
 }
+
